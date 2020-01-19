1aa680328986917f08aa8b7a36212dc7ff09d39c
Aliaksandr Zahatski <github.send@zag.ru>
2020-01-19 22:34:03 +0300

    add load fixtures for hrml export tests

--- t/load-fextures.js
+++ t/load-fextures.js
@@ -2,14 +2,21 @@ const fs = require('fs')
 const glob = require('glob') 
 const allFixures = glob.sync('t/fixtures/*.txt').map( f => {
     const testData = fs.readFileSync(f)
-    testData
     const [ text, json ] = `${testData}`.split('~~~~~~~\n')
-    text
     const tree = JSON.parse(json)
     return { text, tree, file: f}
 })
+
+const allHtmlFixures = glob.sync('t/fixtures-html/*.txt').map( f => {
+    const testData = fs.readFileSync(f)
+    const [ text, json ] = `${testData}`.split('~~~~~~~\n')
+    const tree = json // JSON.parse(json)
+    return { text, tree, file: f}
+})
+
 module.exports = {
-    allFixures  
+    allFixures,
+    allHtmlFixures  
 }
 
 
