c07f76526ce87c3d8fe712ee9a2bec9725adffcc
Aliaksandr Zahatski <github.send@zag.ru>
2020-01-19 22:11:59 +0300

    add tests for html export

---
+++ t/html.test.js
@@ -0,0 +1,21 @@
+
+var toHtml = require('../src').toHtml
+const allFixtures  = require('./load-fextures').allHtmlFixures
+const cleanS = (str) => { return str}
+const toStr = (tree) =>  JSON.stringify(tree ,null, 2)
+
+const prepare = (str)=>cleanS(str)
+describe("run html export tests", () => {
+    allFixtures.map(
+        i => 
+        test(
+                i.file, 
+                ()=> {
+                    const loaded = prepare(i.tree)
+                    const exported = prepare( toStr(toHtml().run(i.text)) )
+                    // expect(exported).toEqual(expect.stringMatching(exported))
+                    expect(exported).toEqual(exported)
+                }
+            )
+)
+})
