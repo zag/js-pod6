324c1ff6a45d8a88e97688c3f190f7a35169184d
Aliaksandr Zahatski <github.send@zag.ru>
2020-01-19 22:13:48 +0300

    tidy code

--- src/plugin-formatting-codes.js
+++ src/plugin-formatting-codes.js
@@ -1,14 +1,15 @@
 'use strict'
 var fcparser = require("../src/grammarfc");
 module.exports = () =>( tree )=>{
-    const visit = (node)=>{
-        if ( Array.isArray(node) ) {
-            node.forEach( i => { visit(i)})
+    const visit = ( node )=>{
+        if ( Array.isArray( node ) ) {
+            node.forEach( i => { visit(i) } )
         } else {
-          if (node.type === 'block') {
-            visit(node.content)
-          } else if (node.type === 'para') {
-              node.content = fcparser.parse(node.text)
+          if ( node.type === 'block' ) {
+            visit( node.content )
+          } else if ( node.type === 'para' ) {
+             // parse text filed for para blocks
+              node.content = fcparser.parse( node.text )
           }
         }
       }
