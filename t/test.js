44d27df16667411356650f9f8f66d83a51111f24
Aliaksandr Zahatski <github.send@zag.ru>
2020-06-14 18:19:50 +0300

    tidy AST for test suite

--- t/test.js
+++ t/test.js
@@ -1,6 +1,8 @@
-var parse = require('../src/').parse
+const clean_plugin = require('./../src/plugin-clean-location')
+var toTree = require('../src/').toTree
+ const cleanTree = toTree().use(clean_plugin)
 const allFixtures  = require('./load-fextures').allFixures
 
 describe("run parser tests", () => {
-    allFixtures.map(i => test(i.file, ()=>expect(parse(i.text)).toEqual( i.tree)))
+    allFixtures.map(i => test(i.file, ()=>expect(cleanTree.parse(i.text)).toEqual( i.tree)))
 })
