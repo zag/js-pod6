c924265ab2b5096e452af42baf7fe7d56206e41e
Aliaksandr Zahatski <github.send@zag.ru>
2021-03-28 21:47:04 +0300

    clean

--- t/test.js
+++ t/test.js
@@ -1,9 +1,6 @@
-
-const clean_plugin = require('./../built/plugin-clean-location').default
 var toTree = require('../built/').toTree
- const cleanTree = toTree().use(clean_plugin)
 const allFixtures  = require('./load-fextures').allFixures
 
 describe("run parser tests", () => {
-    allFixtures.map(i => test(i.file, ()=>expect(cleanTree.parse(i.text)).toEqual( i.tree)))
+    allFixtures.map(i => test(i.file, ()=>expect(toTree().parse(i.text)).toEqual( i.tree)))
 })
