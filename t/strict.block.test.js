24d068d9db70b86b26ad762e2f7bf654fcaf3a0e
Aliaksandr Zahatski <github.send@zag.ru>
2022-10-16 22:18:18 +0200

    fix tests

--- t/strict.block.test.js
+++ t/strict.block.test.js
@@ -188,4 +188,99 @@ test
       ]
     `);
   });
+
+  it("Named blok + margin podMode:1", () => {
+    const tree = parse(
+      `
+        =begin Test
+        s B<ssss>
+        =end some
+         d
+        =end Test
+=para sss`,
+      { podMode: 1, skipChain: 1 }
+    );
+    expect(tree).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "type": "blankline",
+        },
+        Object {
+          "config": Array [],
+          "content": Array [
+            Object {
+              "type": "verbatim",
+              "value": "s B<ssss>
+      =end some
+       d
+      ",
+            },
+          ],
+          "location": Object {
+            "end": Object {
+              "column": 1,
+              "line": 7,
+              "offset": 86,
+            },
+            "start": Object {
+              "column": 1,
+              "line": 2,
+              "offset": 1,
+            },
+          },
+          "margin": "        ",
+          "name": "Test",
+          "text": "        =begin Test
+              s B<ssss>
+              =end some
+               d
+              =end Test
+      ",
+          "type": "block",
+        },
+        Object {
+          "content": Array [
+            Object {
+              "content": Array [
+                Object {
+                  "type": "text",
+                  "value": "sss",
+                },
+              ],
+              "location": Object {
+                "end": Object {
+                  "column": 10,
+                  "line": 7,
+                  "offset": 95,
+                },
+                "start": Object {
+                  "column": 1,
+                  "line": 7,
+                  "offset": 86,
+                },
+              },
+              "margin": "",
+              "text": "sss",
+              "type": "para",
+            },
+          ],
+          "location": Object {
+            "end": Object {
+              "column": 10,
+              "line": 7,
+              "offset": 95,
+            },
+            "start": Object {
+              "column": 1,
+              "line": 7,
+              "offset": 86,
+            },
+          },
+          "margin": "",
+          "name": "para",
+          "type": "block",
+        },
+      ]
+    `);
+  });
 });
