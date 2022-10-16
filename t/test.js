10cb3235c8b456a9f4daab0c40b80797fa0f79f5
Aliaksandr Zahatski <github.send@zag.ru>
2022-10-16 22:16:35 +0200

    remove deprecated tests

--- t/test.js
+++
@@ -1,6 +0,0 @@
-var toTree = require('../built/').toTree
-const allFixtures  = require('./load-fextures').allFixures
-
-describe("run parser tests", () => {
-    allFixtures.map(i => test(i.file, ()=>expect(toTree().parse(i.text)).toEqual( i.tree)))
-})
